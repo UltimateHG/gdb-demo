918cc70783ca2add19be16eaedc135b7fb777935
uhg <26477993+UltimateHG@users.noreply.github.com>
2024-09-12 03:12:00 +0800

    Add files via upload

---
+++ demo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    char* input[100];
+    printf("What is your name?\n");
+    read(stdin, input, 100);
+    printf("Hello %s!\n", input);
+    return 0;
+}
---
+++ demo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int printname(char* name) {
+    printf("Hello %s!\n", name);
+    return 0;
+}
+
+int main() {
+    char* input[100];
+    printf("What is your name?\n");
+    read(stdin, input, 100);
+    printname(input);
+    return 0;
+}
